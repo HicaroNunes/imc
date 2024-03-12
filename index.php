772128ab0df988e06300c279cd662b124c151efc
Hicaro Nunes <nuneshicaro@gmail.com>
2024-03-12 12:16:29 -0300

    0.2
    Adicionado imc para masculino e feminino

--- index.php
+++ index.php
@@ -10,10 +10,15 @@
  <main>
     <form id="form" action="imc.php"method="POST">
         <h2 id="tittle">Indice de massa corporal</h2>
+        <h3>Selecione seu gênero</h3>
+        <select name="genero" id="box">
+            <option value="Masculino">Masculino</option>
+            <option value="Feminino">Feminino</option>
+        </select>
         <h3>Digite a altura</h2>
-        <input id="box" type="number" name="altura" id="altura" placeholder=" ex:1,80" step="0.01">
+        <input id="box" type="number" name="altura" placeholder=" ex:1,80" step="0.01">
         <h3>Digite o peso</h2>
-        <input id="box" type="number" name="peso" id="peso" placeholder=" ex:80">
+        <input id="box" type="number" name="peso" placeholder=" ex:80">
         <br></p>
         <input id="button" type="submit" value="Calcular">
     </form>
